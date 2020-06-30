9f11e76ac3d3eb12fc710bb2f05283389f8198b5
Lucas Brito <lucasbsilva29@gmail.com>
2020-06-30 18:00:53 -0300

    feat(blockchain.py): making validation of block

--- blockchain.py
+++ blockchain.py
@@ -29,6 +29,23 @@ class Blockchain(object):
     def get_prev_block(self):
         return self.chain[-1]
 
+    def proof_of_work(self, prev_proof):
+        new_proof = 1
+        check_proof = False
+
+        while check_proof == False:
+            hash_operation = hashlib.sha256(
+                str(new_proof**2 - prev_proof**2)).hexdigest()
+
+            (check_proof, new_proof) = (
+                True, new_proof) if hash_operation[:4] == "0000" else (False, new_proof + 1)
+
+        return new_proof
+
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+
 
 def main():
     bc = Blockchain()
